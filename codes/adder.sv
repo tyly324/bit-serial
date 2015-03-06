c1f667c3630168307b59d0da9f1cacbfd2c3796a
Zhiyuan.Jiang10 <tyly0324@gmail.com>
2015-03-06 16:39:31 +0000

    debug

--- codes/adder.sv
+++ codes/adder.sv
@@ -5,7 +5,11 @@ module adder(
 	output o_bit_sum,
 	output o_bit_cout);
 
+wire and_ab, and_ac, and_bc;
 assign o_bit_sum = i_bit_a ^ i_bit_b ^ i_bit_cin;
-assign o_bit_cout = (i_bit_a & i_bit_b) | (i_bit_a & i_bit_b) | (i_bit_b & i_bit_cin);
+assign and_ab = i_bit_a & i_bit_b;
+assign and_ac = i_bit_a & i_bit_cin;
+assign and_bc = i_bit_b & i_bit_cin;
+assign o_bit_cout = and_ab | and_ac | and_bc;
 
 endmodule
