3e4263bf6451066e557c241af0e98f98c862a74c
Zhiyuan.Jiang10 <tyly0324@gmail.com>
2015-03-06 16:34:03 +0000

    debug

--- codes/adder.sv
+++ codes/adder.sv
@@ -6,6 +6,6 @@ module adder(
 	output o_bit_cout);
 
 assign o_bit_sum = i_bit_a ^ i_bit_b ^ i_bit_cin;
-assign o_bit_cout = i_bit_a & i_bit_b + i_bit_a & i_bit_b + i_bit_b & i_bit_cin;
+assign o_bit_cout = (i_bit_a & i_bit_b) | (i_bit_a & i_bit_b) | (i_bit_b & i_bit_cin);
 
 endmodule
